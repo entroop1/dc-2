de7b9fe94bee564114e1bd9b70f56238a7515357
bartek <bartek@bonfireit.com>
2018-07-19 16:46:12 +0200

    asd
    ad

---
+++ index.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('logo'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://entroop1.github.io/dc-2/data.json'
+})
